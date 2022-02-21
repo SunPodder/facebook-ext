b55b8cf1f34cdf201f412763b0e7208d331d63a6
SunPodder <contact.sunpodder09@gmail.com>
2022-02-21 21:01:59 +0600

    added posting status

--- src/content.js
+++ src/content.js
@@ -1,26 +1,47 @@
-{
-  setTimeout(function(){
-    if(window.location.href.startsWith("https://m.facebook.com/messages/read/")){
+function sendMessage(){
+  if(window.location.href.startsWith("https://m.facebook.com/messages/read/")){
+    setTimeout(function(){
       let input = document.querySelector("#composerInput")
       let submitBtn = document.getElementsByName("send")[0]
-      let i = 1
+      let i = 1, count = 10
+      let msg = `🥱`
       
       let interval = setInterval(sendMsg, 800)
     
       function sendMsg(){
-        input.value = `Test ${i}`
+        input.value = msg
         setTimeout(function(){
           submitBtn.disabled = false
-          console.log(input.value)
           if(input.value != "") submitBtn.click()
+          else return false
           i++
-          if(i == 5) stop()
+          if(i == (count++)) stop()
         }, 1200)
       }
     
       function stop(){
         clearInterval(interval)
       }
-    }
-  }, 2000)
+    }, 2500)
+  }
 }
+
+function postStatus(){
+  if(!window.location.href == ("https://facebook.com/home.php")) return false
+  setTimeout(function(){
+    document.querySelector("#viewport").style.display = "none"
+    document.querySelector("#composer-placeholder-form").style.display = ""
+    
+    let input = document.querySelector("#composer-placeholder-textarea")
+    let submitBtn = document.querySelector('[name="view_post"]')
+    
+    setTimeout(function(){ input.value = "hello" }, 800)
+    
+    setTimeout(function(){
+      submitBtn.disabled = false
+      submitBtn.click()
+    }, 2000)
+  }, 3000)
+}
+
+postStatus()
